9f1f10ffcdf8e69cb2c93c7c71dfcd16e12a3472
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2020-01-23 19:58:57 +0100

    Expenses: statistics view

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.db.models import Sum, Q
+from django.db.models.functions import ExtractYear, ExtractMonth
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import Category, Expense
+
+
+class StatsView(TemplateView):
+    template_name = "expenses/stats.html"
+
+    def get_context_data(self, **kwargs):
+        q = Expense.objects \
+            .annotate(year=ExtractYear('date'), month=ExtractMonth('date')) \
+            .values('year', 'month') \
+            .annotate(
+                sum_expenses=Sum('amount', filter=Q(income=False)),
+                sum_incomes=Sum('amount', filter=Q(income=True)),
+            ) \
+            .order_by()
+        return dict(results=q)
