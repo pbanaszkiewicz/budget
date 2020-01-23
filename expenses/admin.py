d9000330ed02bacbe9ae2d05dfc1207ca7fb7933
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2020-01-23 19:58:33 +0100

    Expenses: admin views

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Expense
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+    search_fields = ["name"]
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+    date_hierarchy = "date"
+    list_display = ["description", "shop", "date", "category", "amount", "income"]
+    list_filter = ["shop", "date", "category"]
+    list_select_related = True
+    autocomplete_fields = ["category"]
+    search_fields = ["description", "shop", "category"]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Expense, ExpenseAdmin)
