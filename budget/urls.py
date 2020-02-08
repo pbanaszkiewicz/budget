a3ea99223c90e4e38cfadaf88d15cbfae8699062
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2020-02-08 13:40:18 +0100

    Login/logout views

--- budget/urls.py
+++ budget/urls.py
@@ -1,10 +1,14 @@
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('expenses.urls')),
+    #
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 if settings.DEBUG:
